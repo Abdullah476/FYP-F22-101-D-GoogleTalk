07e709472219be6088bd85e615c07f71f90bb921
IcyGuy18 <90891688+IcyGuy18@users.noreply.github.com>
2023-06-10 22:31:01 +0500

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
 import subprocess
 import mlflow.spacy
+import spacy
+import os
 
-subprocess.call(["python", "-m", "spacy", "init", "config", "config.cfg", "--lang", "en", "--pipeline", "ner", "--optimize", "accuracy", "--force"])
-subprocess.call(["python", "-m", "spacy", "train", "config.cfg", "--output", "./model/", "--paths.train", "./training_data.spacy", "--paths.dev", "./validation_data.spacy"])
+subprocess.call(["py", "-m", "spacy", "init", "config", "config.cfg", "--lang", "en", "--pipeline", "ner", "--optimize", "accuracy", "--force"])
+
+subprocess.call(["py", "-m", "spacy", "train", "config.cfg", "--output", "./model/", "--paths.train", "./training_data.spacy", "--paths.dev", "./validation_data.spacy"])
 
 nlp_ner = spacy.load(os.getcwd() + "/model/model-last")
 
-mlflow.spacy.save_model(spacy_model=nlp_ner, artifact_path="./mlflow_artifacts/")
+mlflow.spacy.save_model(spacy_model=nlp_ner, path="./mlflow_artifacts/" + )
